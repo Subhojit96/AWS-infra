4214ba74385db748a2000b5a275c5b43bc41371b
Subhajit <subhojit96@outlook.com>
2021-10-28 08:54:59 +0530

    push

--- provider.tf
+++ provider.tf
@@ -8,11 +8,4 @@ terraform {
 }
 
 provider "aws" {
-<<<<<<< HEAD
-=======
-  profile ="default"
-  access_key = "${var.access_key}"
-  secret_key = "${var.secret_key}"
->>>>>>> 94e18901e92aa9f50fd205cbf4089d3c506f4c03
-  region="${var.region}"
 }
--- variable.tf
+++ variable.tf
@@ -2,23 +2,17 @@
 variable "access_key" {
   description = "Enter access key"
   type = string
-  default "AKIAZJ4SL3S7EA5PFIPX"
 }
 
 variable "secret_key" {
   type= string
   description = "Enter secret_key"
-  default "BK8tUl4CrY5JkCdEN3uQ8HIdQs9pnULRtIp+ObQR "
 }
 
 variable "region" {
   type = string
   description = "Enter aws region"
-<<<<<<< HEAD
-  
-=======
-  default = "us=east-1"
->>>>>>> 94e18901e92aa9f50fd205cbf4089d3c506f4c03
+
 }
 
 variable "cidr_vpc" {
