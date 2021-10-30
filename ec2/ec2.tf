f79ed835721cdffecc04fd43208422d369f1d2f6
Subhajit <subhojit96@outlook.com>
2021-10-30 08:45:57 +0530

    chngeFolder

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,9 +1,9 @@
-resource "aws_instance" "ec2-1" {
+resource "aws_instance" "ec2" {
   ami = "ami-02e136e904f3da870"
   tenancy = "default"
   instance_type = "t2.micro"
   instance_initiated_shutdown_behavior = "stop"
-  key_name = "${aws_key_pair.key.key_name}"
+  //key_name = "${aws_key_pair.key.key_name}"
   monitoring = false
   //count = "${length(var.public_subnet_cidr)}"
   subnet_id = aws_subnet.public.0.id
@@ -14,11 +14,12 @@ resource "aws_instance" "ec2-1" {
 }
 
 #LoginKey Details
-resource "aws_key_pair" "key" {
+/*resource "aws_key_pair" "key" {
   key_name = "Jenkins-pem"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDjWUdEzt8z9wyO4lpBZrdPwEEvRuBqh0Lo1+SAI1obdojIXj27y0ZAAuOmIpJzUONnP/xcTUKWbRwT0RGbz/QGwaxX/Zfj21XH0M6NWEOhu7TIgzFNApSZKbY8vEcA4gcQXSCMjbEmjrFthDDiK9CAUqp/uV95H9sJdi2FZ1WCuISvtIrPeJ7RT5JmFLPIltuSATwV9Xu29xtWEeXfnLqip9qj7snB5eWaNkuqGLjAfdXHaaInG8Q/J7vyrhD2RG44mPz8absVMOt2xNBSkiDmN9bawaShzEU7GjAyWgLGJ2SMRsma0X7M0429ADQ3THiEIL7r8C6ENV6QpCE1mgWn Jenkins"
-}
+}*/
 resource "aws_ebs_volume" "ebs" {
   size = "8"
+ // count = "${length(var.availability_zone) }"
   availability_zone = var.availability_zone.0
 }
---
+++ ec2/variable.tf
@@ -0,0 +1,36 @@
+
+variable "access_key" {
+  description = "Enter access key"
+  type = string
+  
+}
+
+variable "secret_key" {
+  type= string
+  description = "Enter secret_key"
+  
+}
+variable "cidr_vpc" {
+  description = "Enter CIDR range"
+  default = "192.168.0.0/16"
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = list
+  default = ["192.168.4.0/24","192.168.3.0/24"]
+}
+
+variable "private_subnet_cidr" {
+  type = list
+  default = ["192.168.5.0/24", "192.168.6.0/24"]
+}
+variable "availability_zone" {
+  type = list
+  default = ["us-east-1a", "us-east-1b"]
+}
+
+variable "tag" {
+  type = string
+  default = "Dev-cvayutopia"
+}
